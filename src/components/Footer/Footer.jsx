ea150cc7859058320c6a4fad625a0a18e3613a85
Matias Alvez <matias2018alvez@gmail.com>
2023-03-27 14:58:35 -0300

    prueba

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <h1>PROBANDOOOOOOO</h1>
+    <div className='footer'>
+        <ul>
+            <li>Haz</li>
+            <li>&</li>
+            <li>Enves</li>
+        </ul>
     </div>
   );
 };
