459c2770f74c1c44d8c89a349788e9b999c1a6d3
Matias Alvez <matias2018alvez@gmail.com>
2023-03-27 15:41:53 -0300

    versionListaParaUsar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <div className='footer'>
-        HAZYENVES
+      <div> HAZ & ENVES </div>
+      <img src="https://lumiere-a.akamaihd.net/v1/images/og_cars_lightningmcqueenday_18244_4435f27a.jpeg?region=40,0,1120,630&width=768" alt="imagen" />
     </div>
   );
 };
