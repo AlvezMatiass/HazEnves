ae2818d45a2fa013d8ac74e9b34c2df9e2d71d24
Ignacio Calace <ignacio.calace@gmail.com>
2023-04-17 14:32:00 -0300

    carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import './App.css'
+import "./App.css";
+import Contacto from "./components/Contacto/Contacto.jsx";
 
-import Footer from './components/Footer/Footer'
-import Inicio from './components/Inicio/Inicio'
-import Nosotros from './components/Nosotros/Nosotros'
-import Participacion from './components/Participacion/Participacion'
-import Presentacion from './components/Presentacion/Presentacion'
-import Proceso from './components/Proceso/Proceso'
-import Servicio from './components/Servicio/Servicio'
+import Footer from "./components/Footer/Footer";
+import Inicio from "./components/Inicio/Inicio";
+import Nosotros from "./components/Nosotros/Nosotros";
+import Participacion from "./components/Participacion/Participacion";
+import Presentacion from "./components/Presentacion/Presentacion";
+import Proceso from "./components/Proceso/Proceso";
+import Servicio from "./components/Servicio/Servicio";
+import Carrousel from "./components/Carrousel/Carrousel";
 
 function App() {
-
   return (
     <div className="App">
-      <Inicio/>
+      <Inicio />
 
-      <Nosotros/>
+      <Nosotros />
 
-      <Proceso/>
+      <Proceso />
 
-      <Presentacion/>
+      <Presentacion />
 
-      <Servicio/>
+      <Servicio />
 
-      <Participacion/>
+      <Participacion />
 
-      <Contacto/>
+      <Carrousel />
 
-      <Footer/>
+      <Contacto />
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
